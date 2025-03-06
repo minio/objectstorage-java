balamurugana/minio-java

Refactor messages by subclasses
merge SelectObjectContentRequest
merge ListVersionsResult
Remove unused CompleteMultipartUploadOutput.java
merge ListMultipartUploadsResult
merge NotificationRecords
merge SseConfiguration
merge DeleteRequest, DeleteResult
merge RestoreRequest
merge NotificationConfiguration
merge LifecycleConfiguration
merge ReplicationConfiguration
merge Filter
merge ObjectLockConfiguration
merge ListAllMyBucketsResult
merge Item
merge ListObjectsResult
Move common fields for ListPartsResult and GetObjectAttributesOutput

Signed-off-by: Bala.FA <bala@minio.io>

api/src/main/java/io/minio/messages/DeleteRequest.java
@@ -16,7 +16,9 @@
 
 package io.minio.messages;
 
+import io.minio.Time;
 import io.minio.Utils;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Objects;
 import javax.annotation.Nonnull;
@@ -24,6 +26,10 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Namespace;
 import org.simpleframework.xml.Root;
+import org.simpleframework.xml.convert.Convert;
+import org.simpleframework.xml.convert.Converter;
+import org.simpleframework.xml.stream.InputNode;
+import org.simpleframework.xml.stream.OutputNode;
 
 /**
  * Object representation of request XML of <a
@@ -38,12 +44,81 @@ public class DeleteRequest {
   private boolean quiet;
 
   @ElementList(name = "Object", inline = true)
-  private List<DeleteObject> objectList;
+  private List<Object> objectList;
 
   /** Constructs new delete request for given object list and quiet flag. */
-  public DeleteRequest(@Nonnull List<DeleteObject> objectList, boolean quiet) {
+  public DeleteRequest(@Nonnull List<Object> objectList, boolean quiet) {
     this.objectList =
         Utils.unmodifiableList(Objects.requireNonNull(objectList, "Object list must not be null"));
     this.quiet = quiet;
+  }
+
+  @Root(name = "Object")
+  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "URF_UNREAD_FIELD")
+  public static class Object {
+    @Element(name = "Key")
+    private String name;
+
+    @Element(name = "VersionId", required = false)
+    private String versionId;
+
+    @Element(name = "ETag", required = false)
+    private String etag;
+
+    @Element(name = "LastModifiedTime", required = false)
+    private HttpHeaderDate lastModifiedTime;
+
+    @Element(name = "Size", required = false)
+    private Long size;
+
+    public Object(String name) {
+      this.name = name;
+    }
+
+    public Object(String name, String versionId) {
+      this(name);
+      this.versionId = versionId;
+    }
+
+    public Object(
+        String name, String versionId, String etag, ZonedDateTime lastModifiedTime, Long size) {
+      this(name, versionId);
+      this.etag = etag;
+      this.lastModifiedTime =
+          lastModifiedTime == null ? null : new HttpHeaderDate(lastModifiedTime);
+      this.size = size;
+    }
+
+    /** HTTP header date wrapping {@link ZonedDateTime}. */
+    @Root
+    @Convert(HttpHeaderDate.HttpHeaderDateConverter.class)
+    public static class HttpHeaderDate {
+      private ZonedDateTime zonedDateTime;
+
+      public HttpHeaderDate(ZonedDateTime zonedDateTime) {
+        this.zonedDateTime = zonedDateTime;
+      }
+
+      public String toString() {
+        return zonedDateTime.format(Time.HTTP_HEADER_DATE_FORMAT);
+      }
+
+      public static HttpHeaderDate fromString(String dateString) {
+        return new HttpHeaderDate(ZonedDateTime.parse(dateString, Time.HTTP_HEADER_DATE_FORMAT));
+      }
+
+      /** XML converter class. */
+      public static class HttpHeaderDateConverter implements Converter<HttpHeaderDate> {
+        @Override
+        public HttpHeaderDate read(InputNode node) throws Exception {
+          return HttpHeaderDate.fromString(node.getValue());
+        }
+
+        @Override
+        public void write(OutputNode node, HttpHeaderDate date) {
+          node.setValue(date.toString());
+        }
+      }
+    }
   }
 }

minio/objectstorage-java

api/src/main/java/io/minio/messages/DeleteRequest.java
@@ -16,7 +16,9 @@
 
 package io.minio.messages;
 
+import io.minio.Time;
 import io.minio.Utils;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Objects;
 import javax.annotation.Nonnull;
@@ -24,6 +26,10 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Namespace;
 import org.simpleframework.xml.Root;
+import org.simpleframework.xml.convert.Convert;
+import org.simpleframework.xml.convert.Converter;
+import org.simpleframework.xml.stream.InputNode;
+import org.simpleframework.xml.stream.OutputNode;
 
 /**
  * Object representation of request XML of <a
@@ -38,12 +44,81 @@ public class DeleteRequest {
   private boolean quiet;
 
   @ElementList(name = "Object", inline = true)
-  private List<DeleteObject> objectList;
+  private List<Object> objects;
 
   /** Constructs new delete request for given object list and quiet flag. */
-  public DeleteRequest(@Nonnull List<DeleteObject> objectList, boolean quiet) {
-    this.objectList =
-        Utils.unmodifiableList(Objects.requireNonNull(objectList, "Object list must not be null"));
+  public DeleteRequest(@Nonnull List<Object> objects, boolean quiet) {
+    this.objects =
+        Utils.unmodifiableList(Objects.requireNonNull(objects, "Object list must not be null"));
     this.quiet = quiet;
+  }
+
+  @Root(name = "Object")
+  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "URF_UNREAD_FIELD")
+  public static class Object {
+    @Element(name = "Key")
+    private String name;
+
+    @Element(name = "VersionId", required = false)
+    private String versionId;
+
+    @Element(name = "ETag", required = false)
+    private String etag;
+
+    @Element(name = "LastModifiedTime", required = false)
+    private HttpHeaderDate lastModifiedTime;
+
+    @Element(name = "Size", required = false)
+    private Long size;
+
+    public Object(String name) {
+      this.name = name;
+    }
+
+    public Object(String name, String versionId) {
+      this(name);
+      this.versionId = versionId;
+    }
+
+    public Object(
+        String name, String versionId, String etag, ZonedDateTime lastModifiedTime, Long size) {
+      this(name, versionId);
+      this.etag = etag;
+      this.lastModifiedTime =
+          lastModifiedTime == null ? null : new HttpHeaderDate(lastModifiedTime);
+      this.size = size;
+    }
+
+    /** HTTP header date wrapping {@link ZonedDateTime}. */
+    @Root
+    @Convert(HttpHeaderDate.HttpHeaderDateConverter.class)
+    public static class HttpHeaderDate {
+      private ZonedDateTime zonedDateTime;
+
+      public HttpHeaderDate(ZonedDateTime zonedDateTime) {
+        this.zonedDateTime = zonedDateTime;
+      }
+
+      public String toString() {
+        return zonedDateTime.format(Time.HTTP_HEADER_DATE_FORMAT);
+      }
+
+      public static HttpHeaderDate fromString(String dateString) {
+        return new HttpHeaderDate(ZonedDateTime.parse(dateString, Time.HTTP_HEADER_DATE_FORMAT));
+      }
+
+      /** XML converter class. */
+      public static class HttpHeaderDateConverter implements Converter<HttpHeaderDate> {
+        @Override
+        public HttpHeaderDate read(InputNode node) throws Exception {
+          return HttpHeaderDate.fromString(node.getValue());
+        }
+
+        @Override
+        public void write(OutputNode node, HttpHeaderDate date) {
+          node.setValue(date.toString());
+        }
+      }
+    }
   }
 }